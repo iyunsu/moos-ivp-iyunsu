aed54b93555548669a5844f41b277216f5be5a9c
iyunsu <44156373+iyunsu@users.noreply.github.com>
2020-05-09 22:46:04 +0800

    backup_0509

---
+++ build/src/lib_behaviors-iyunsu/CMakeFiles/BHV_PoseKeeping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BHV_PoseKeeping.dir/BHV_PoseKeeping.cpp.o"
+  "../../../lib/libBHV_PoseKeeping.pdb"
+  "../../../lib/libBHV_PoseKeeping.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BHV_PoseKeeping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
